a073ac07c9020a29d6b9f3edd46066d38d22edf8
Michela <micheladebellis@gmail.com>
2024-03-29 14:40:45 +0100

    Initial commit

---
+++ mealManager/src/main/java/com/projects/mealManager/MealManagerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.mealManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealManagerApplication.class, args);
+	}
+
+}
