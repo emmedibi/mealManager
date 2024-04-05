88c4f8c21b21ac40d481955ce02c3c36e465ba9e
Michela <micheladebellis@gmail.com>
2024-04-05 14:23:07 +0200

    Added Json patter for LocalDateTime and the controller's methods for creating a meal from a list of recipes.

--- mealManager/src/main/java/com/projects/mealManager/controller/MealController.java
+++ mealManager/src/main/java/com/projects/mealManager/controller/MealController.java
@@ -5,19 +5,20 @@ import com.projects.mealManager.entity.Recipe;
 import com.projects.mealManager.service.MealService;
 import com.projects.mealManager.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/meals")
 @RequiredArgsConstructor
+@Slf4j
 public class MealController {
 
     @Autowired
@@ -25,14 +26,31 @@ public class MealController {
     RecipeService recipeService;
 
     @GetMapping
-    public ResponseEntity<Page<Recipe>> getRecipes(@RequestParam(value="page", defaultValue= "0") int page,
+    public ResponseEntity<Page<Meal>> getMeals(@RequestParam(value="page", defaultValue= "0") int page,
                                                    @RequestParam(value="size", defaultValue = "10") int size){
-        return ResponseEntity.ok().body(recipeService.getAllRecipes(page, size));
+        return ResponseEntity.ok().body(mealService.getAllMeals(page, size));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Recipe> getRecipes(@PathVariable(value="id") String id){
-        return ResponseEntity.ok().body(recipeService.getRecipe(id));
+    public ResponseEntity<Meal> getMeal(@PathVariable(value="id") String id){
+        return ResponseEntity.ok().body(mealService.getMeal(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Meal> addMeal(@RequestParam(value="recipeIds") List<String> recipeIds){
+        Meal meal = new Meal();
+        try {
+            ArrayList<Recipe> recipeList = new ArrayList<>();
+            for(String stringIdIterator : recipeIds){
+                recipeList.add(recipeService.getRecipe(stringIdIterator));
+            }
+            meal.setRecipes(recipeList);
+            LocalDateTime mealDateTime = LocalDateTime.now();
+            meal.setMealDateTime(mealDateTime);
+        } catch (Exception e){
+            log.info(e.getMessage());
+        }
+        return ResponseEntity.ok().body(mealService.createMeal(meal));
     }
 
 }
--- mealManager/src/main/java/com/projects/mealManager/entity/Meal.java
+++ mealManager/src/main/java/com/projects/mealManager/entity/Meal.java
@@ -1,6 +1,9 @@
 package com.projects.mealManager.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,13 +27,15 @@ public class Meal {
     @UuidGenerator
     @Column(name="id",unique = true, updatable = false)
     private String id;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime mealDateTime;
 
     @ManyToMany(mappedBy = "mealSet")
     ArrayList<Recipe> recipes;
 
     @ManyToOne
-    @JoinColumn(name="weeklyPlan_id", nullable=false)
+    @JoinColumn(name="weeklyPlan_id")
     private WeeklyPlan weeklyPlan;
 
 }
